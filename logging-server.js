e9d3c28624c4a557191bab260678be11f17baa07
kajetant <kajetan.targonski@gmail.com>
2019-01-31 14:11:59 +0100

    added passing data to servers

--- logging-server.js
+++ logging-server.js
@@ -2,27 +2,60 @@ var http = require('http');
 var logger = require('./logging').logger
 var endOfLine = require('os').EOL;
 
+const msgPath = '/api/data-feed/betgenius/message';
+
+const hosts = [{ hostname: 'localhost', port: 8301 }];
+
 var server = http.createServer(function (request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.writeHead(200, { "Content-Type": "text/plain" });
     response.end();
 
-    logger.log({level: 'info', message: request.url});
+    logger.log({ level: 'info', message: request.url });
 
-    if(request.url === '/api/data-feed/betgenius/message'){
+    if (request.url === msgPath) {
         wholeBody = '';
 
         request.on('data', chunk => {
             wholeBody += chunk.toString();
         });
 
-        request.on('end', ()=> {
-           logger.log({level: 'info', message: endOfLine + wholeBody})
+        request.on('end', () => {
+            logger.log({ level: 'info', message: endOfLine + wholeBody })
+
+            if (wholeBody) {
+
+                hosts.forEach(host => {
+
+                    try {
+                        const options = {
+                            hostname: host.hostname,
+                            port: host.port,
+                            path: msgPath,
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json',
+                                'Content-Length': wholeBody.length,
+                                'Authorization': 'Basic token-here!'
+                            }
+                        };
+
+                        let req = http.request(options, (res) => {
+                            console.log(`statusCode: ${res.statusCode}`);
+                        });
+
+                        req.write(wholeBody);
+                        req.end();
+
+                    } catch(ex) {
+
+                    }
+                });
+            }
         });
     }
-
 });
 
-var port = 3000;
+var port = 5555;
 
 server.listen(port);
 
